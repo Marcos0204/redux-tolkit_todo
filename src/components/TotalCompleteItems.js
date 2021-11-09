b79349bba2817b2901ae1aed6f8f969d9f2cc7c4
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-09 20:13:51 +0100

    update-todos-length-complete

--- src/components/TotalCompleteItems.js
+++ src/components/TotalCompleteItems.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
 
 const TotalCompleteItems = () => {
-	return <h4 className='mt-3'>Total Complete Items: 5</h4>;
+	const todosCompleted = useSelector(
+		({todos})=> todos.filter(item => item.completed === true))
+	return <h4 className='mt-3'>Total Complete Items: {todosCompleted.length}</h4>;
 };
 
 export default TotalCompleteItems;
